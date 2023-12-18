30dd4b4ff8c91da54c42b774588fc7b2b952a368
YourmixJNR <radmakson@gmail.com>
2023-12-18 20:12:09 +0100

    Developers Section

---
+++ src/Components/Developers/Developers.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Developers = () => {
+  return (
+    <div>
+      {/* <!-- Section: Developers --> */}
+      <section class="section section-developers white-text">
+        <div class="primary-overlay valign-wrapper">
+          <div class="container">
+            <div class="row">
+              <div class="col s12 center">
+                <h3>For Developers, By Developers</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Developers;
