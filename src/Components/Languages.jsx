33d791b1481ed42b53a69651d52e3726eabe3bf3
YourmixJNR <radmakson@gmail.com>
2023-12-18 20:31:07 +0100

    Languages Section

--- src/Components/Languages.jsx
+++ src/Components/Languages.jsx
@@ -3,33 +3,33 @@ import React from "react";
 const Languages = () => {
   return (
     // <!-- Section: Languages -->
-    <section class="section section-language grey lighten-4">
-      <div class="container">
-        <div class="row">
-          <h4 class="center">
+    <section className="section section-language grey lighten-4">
+      <div className="container">
+        <div className="row">
+          <h4 className="center">
             Work With
-            <span class="deep-purple-text text-darken-1"> Any Language</span>
+            <span className="deep-purple-text text-darken-1"> Any Language</span>
           </h4>
           <br />
           <br />
-          <div class="row">
-            <div class="col s2">
-              <img src="/Images/python-logo.png" class="responsive-img" alt="" />
+          <div className="row">
+            <div className="col s2">
+              <img src="/Images/python-logo.png" className="responsive-img" alt="" />
             </div>
-            <div class="col s2">
-              <img src="/Images/ruby-logo.png" class="responsive-img" alt="" />
+            <div className="col s2">
+              <img src="/Images/ruby-logo.png" className="responsive-img" alt="" />
             </div>
-            <div class="col s2">
-              <img src="/Images/php-logo.png" class="responsive-img" alt="" />
+            <div className="col s2">
+              <img src="/Images/php-logo.png" className="responsive-img" alt="" />
             </div>
-            <div class="col s2">
-              <img src="/Images/node-logo.png" class="responsive-img" alt="" />
+            <div className="col s2">
+              <img src="/Images/node-logo.png" className="responsive-img" alt="" />
             </div>
-            <div class="col s2">
-              <img src="/Images/c-sharp-logo.png" class="responsive-img" alt="" />
+            <div className="col s2">
+              <img src="/Images/c-sharp-logo.png" className="responsive-img" alt="" />
             </div>
-            <div class="col s2">
-              <img src="/Images/java-logo.png" class="responsive-img" alt="" />
+            <div className="col s2">
+              <img src="/Images/java-logo.png" className="responsive-img" alt="" />
             </div>
           </div>
         </div>
