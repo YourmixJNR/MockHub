d2d335ca246683a19e768278fd007b90a3f5d1e2
YourmixJNR <radmakson@gmail.com>
2023-12-18 20:03:44 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Layouts/Header/Header';
 import HeroSection from './Components/HeroSection/HeroSection';
+import IconBoxes from './Components/IconBoxes';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <HeroSection />
+      <IconBoxes />
     </div>
   );
 }
