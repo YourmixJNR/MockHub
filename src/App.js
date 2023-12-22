2df4ea0b07fa878c65d25cce028b26fdc3f4c623
YourmixJNR <radmakson@gmail.com>
2023-12-22 19:24:01 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
 import Solutions from './Pages/Solutions';
@@ -6,15 +6,10 @@ import Solutions from './Pages/Solutions';
 function App() {
   return (
     <BrowserRouter>
-    {/* <Router> */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/solutions' element={<Solutions />} />
       </Routes>
-    {/* </Router> */}
-    
-      {/* <Home /> */}
-    
     </BrowserRouter>
   );
 }
---
+++ src/Components/Welcome/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Welcome = () => {
+  return (
+    <div class="showcase container">
+      <div class="row">
+        <div class="col s12 m10 offset-m1 center">
+          <h5>Welcome To Quazzu</h5>
+          <h1>Cloud Solutions</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
